17ca324aea7b3ff8e5ff99eda02019b4493859e6
weichenhao <1135361471@qq.com>
2024-05-14 14:27:32 +0800

    启用禁用员工账号业务代码

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -27,4 +27,7 @@ public interface EmployeeMapper {
 
     //分页查询
     Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    //根据主键动态修改属性
+    void update(Employee employee);
 }
